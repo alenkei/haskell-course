5c4f1ea7dc483f9573f1aebbd96b2108c23185c3
Andrei Lenkei <114986032+alenkei-pcp@users.noreply.github.com>
2024-05-28 13:44:22 +0200

    Feature: Adjust for IntelliJ
    - Added an app/Main.hs as a main app
    - Added minimal stack.yaml
    - Added minimal haskell-course.cabal for app
    - Use "stack build --test --haddock --no-haddock-hyperlink-source" to build from PowerShell
    - See "stack init" to initialize a new project

--- Homework/Homework01/Homework01.hs
+++ Homework/Homework01/Homework01.hs
@@ -8,12 +8,12 @@ multiplyBy3 x = x*3
 
 -- Question 3
 -- Define a function that calculates the area of a circle.
-circleArea r = pi * (r^2)
+circleArea r = pi * r^2
 
 -- Question 4
 -- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder. 
-cylinderVolume r h =  (circleArea r) * h
+cylinderVolume r h =  circleArea r * h
 
 -- Question 5
 -- Define a function that takes the height and radius of a cylinder and checks if the volume is greater than or equal to 42.
-volumeGorE42 r h = (cylinderVolume r h) >= 42
+volumeGorE42 r h = cylinderVolume r h >= 42
