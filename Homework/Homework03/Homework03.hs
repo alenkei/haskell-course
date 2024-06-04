79a5f76e8a6d69b5be9d96d79047e2d4bee23a02
Andrei Lenkei <114986032+alenkei-pcp@users.noreply.github.com>
2024-06-04 17:56:48 +0200

    Add Hw03:Q4 solution

--- Homework/Homework03/Homework03.hs
+++ Homework/Homework03/Homework03.hs
@@ -16,6 +16,12 @@ mc c h m
 -- So `show 3` will produce `"3"` and `show (3 > 2)` will produce `"True"`.
 
 -- In the previous function, return the excess/savings of consumption as part of the message.
+mc' c h m
+  | mu > m = "Consumption was greater by " ++ show (mu - m)
+  | mu < m = "Consumption was smaller: " ++ show (m - mu)
+  | otherwise = "Consumption was equal"
+  where
+    mu = c * h * 30
 
 
 -- Question 3
@@ -26,8 +32,12 @@ mc c h m
 -- Question 4
 -- Write a function that takes in two numbers and returns their quotient such that it is not greater than 1.
 -- Return the number as a string, and in case the divisor is 0, return a message why the division is not
--- possible. To implement this function using both guards and if-then-else statements.  
-
+-- possible. To implement this function using both guards and if-then-else statements.
+lq :: Double -> Double -> String
+lq a b
+  | a == 0 && b > a = "Cannot divide by zero"
+  | b == 0 && a > b = "Cannot divide by zero"
+  | otherwise = if a < b then show (a/b) else show (b/a)
 
 -- Question 5
 -- Write a function that takes in two numbers and calculates the sum of square roots for the product and quotient
